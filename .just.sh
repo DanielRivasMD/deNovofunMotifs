4c1e6607b26148b878a62f649d2f85b8e69321f2
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-12 18:12:30 +0200

    add just protocols

---
+++ .just.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+####################################################################################################
+
+# remote
+projDir="/crex/proj/snic2020-16-187/nobackup"
+funMotifsAnalysis="${projDir}/funMotifs_analysis"
+siqiDir="${funMotifsAnalysis}/Siqi_files"
+
+# Uppmax
+uppmaxID="drivas@rackham.uppmax.uu.se"
+
+####################################################################################################
