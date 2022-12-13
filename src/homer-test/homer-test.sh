6ecd4ad71479a4c8bc7631e78671cffc1668779a
Manel Mateos i Font <mama9758@rackham2.uppmax.uu.se>
2022-12-13 11:23:51 +0100

    more homer test

--- src/homer-test/homer-test.sh
+++ src/homer-test/homer-test.sh
@@ -13,5 +13,7 @@ module load bioinfo-tools
 module load HOMER
 
 #homer2 denovo -i chr10_mutated.fa -b JASPAR2022_CORE_non-redundant_pfms_meme.txt > out.txt
-findMotifs.pl data/chr10_mutated_siqi.fa human data/analysis_output/ -fasta data/JASPAR2022_CORE_non-redundant_pfms_meme.txt
+#findMotifs.pl  human data/analysis_output/ -fasta data/chr10_mutated_siqi.fa > out.txt
+#findMotifs.pl data/test.fa human data/analysis_output/ -fasta -find data/JASPAR2022_CORE_non-redundant_pfms_meme.txt > find_result_m.motif
+findMotifs.pl data/head.fa fasta data/result_dir_head/ -fasta data/bg.fa -find data/JASPAR.txt > my_result_head.txt
 
