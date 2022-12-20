e485cbdd5b274095fe543d5b5e47ea3a049e97ae
ersanchezo <ersanchezo@hotmail.com>
2022-12-20 19:55:13 +0100

    Add files via upload

--- src/HomeR.R
+++ src/HomeR.R
@@ -10,6 +10,8 @@ library(Biostrings)
 
 library(BiocParallel)
 
+gen_mut<- readDNAStringSet('data/chr21.extended_motifs.fa.mutated.uniq', format="fasta", nrec=-1L, skip=0L, seek.first.rec=FALSE, use.names=TRUE)
+
 pfms <- getMatrixSet(JASPAR2022,
                      opts = list(matrixtype = "PFM",
                                  tax_group = "vertebrates"))
